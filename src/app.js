72a9a45d17daa6e424be6ed983e0852de301b782
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-05 21:01:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let redOnes = ["♦", "♥"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function createSN() {
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const randomNumber = Math.floor(Math.random() * number.length);
+    const suit = suits[randomSuits];
+    const symbolElement = document.getElementById("SIsymbol");
+    const symbolElement2 = document.getElementById("SIsymbol2");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    symbolElement.innerHTML = suit;
+    symbolElement2.innerHTML = suit;
+
+    if (redOnes.includes(suit)) {
+      symbolElement.style.color = "red";
+      symbolElement2.style.color = "red";
+    } else {
+      symbolElement.style.color = "black";
+      symbolElement2.style.color = "black";
+    }
+
+    document.getElementById("numb3r").innerHTML = number[randomNumber];
+  }
+
+  createSN();
 };
